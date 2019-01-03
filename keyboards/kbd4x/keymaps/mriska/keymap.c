da3db5c3c263654778d1f47a9810659870f8ee31
Mikael Riska <mikael.riska@ecraft.com>
2019-01-03 09:00:49 +0100

    Move right shift to where it should be

--- keyboards/kbd4x/keymaps/mriska/keymap.c
+++ keyboards/kbd4x/keymaps/mriska/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_ortho_4x12(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,                KC_Y,               KC_U,      KC_I,    KC_O,    KC_P,    KC_QUOT,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,       KC_G,                KC_H,               KC_J,      KC_K,    KC_L,    KC_SCLN, KC_ENTER,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,                KC_N,               KC_M,      KC_COMM, KC_DOT,  KC_RSFT, ___,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,                KC_N,               KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
     ___,     KC_LCTL, KC_LALT, KC_LGUI, MO(_THREE), LT(_ARROW, KC_BSPC), LT(_NUM, KC_SPACE), MO(_FOUR), KC_RGUI, KC_RALT, KC_RCTL, ___
   ),
 
