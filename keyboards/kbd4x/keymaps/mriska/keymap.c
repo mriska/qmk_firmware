06d798cd09a4742b5332396a47925219219a5c90
Mikael Riska <mikael.riska@ecraft.com>
2019-01-21 22:40:32 +0100

    More keycodes

--- keyboards/kbd4x/keymaps/mriska/keymap.c
+++ keyboards/kbd4x/keymaps/mriska/keymap.c
@@ -36,31 +36,31 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_ortho_4x12(
-    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,                KC_Y,               KC_U,      KC_I,    KC_O,    KC_P,    KC_QUOT,
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,       KC_G,                KC_H,               KC_J,      KC_K,    KC_L,    KC_SCLN, KC_ENTER,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,                KC_N,               KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    ___,     KC_LCTL, KC_LALT, KC_LGUI, MO(_THREE), LT(_ARROW, KC_BSPC), LT(_NUM, KC_SPACE), MO(_FOUR), KC_RGUI, KC_RALT, KC_RCTL, ___
+    KC_ESC,     KC_Q, KC_W,    KC_E,    KC_R,    KC_T,                KC_Y,               KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
+    KC_TAB,     KC_A, KC_S,    KC_D,    KC_F,    KC_G,                KC_H,               KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENTER,
+    KC_LSFT,    KC_Z, KC_X,    KC_C,    KC_V,    KC_B,                KC_N,               KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    MO(_THREE), ___,  KC_LCTL, KC_LALT, KC_LGUI, LT(_ARROW, KC_BSPC), LT(_NUM, KC_SPACE), KC_RGUI, KC_RALT, KC_RCTL, ___,     MO(_FOUR)
   ),
 
   [_NUM] = LAYOUT_ortho_4x12(
-    ___,      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  ___,
-    ___,      KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    ___,
-    ___,      S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), ___,
-    ___,      ___,     ___,     ___,     ___,     ___,     ___,     ___,     ___,     ___,     ___,     ___
+    KC_GRAVE,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DELETE,
+    KC_EQUAL,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINUS,
+    S(KC_EQUAL), S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), S(KC_MINUS),
+    ___,         ___,     ___,     ___,     ___,     ___,     ___,     ___,     ___,     ___,     ___,     ___
   ),
 
   [_ARROW] = LAYOUT_ortho_4x12(
-    ___,      ___,     ___,     ___,     ___,     ___,  ___,  ___,     KC_UP,   ___,     ___,  ARING,
-    ___,      ___,     ___,     ___,     ___,     ___,  ___,  KC_LEFT, KC_DOWN, KC_DOWN, ODIA, ADIA,
-    ___,      ___,     ___,     ___,     ___,     ___,  ___,  ___,     ___,     ___,     ___,  ___,
-    ___,      ___,     ___,     ___,     ___,     ___,  ___,  ___,     ___,     ___,     ___,  ___
+    ___,      ___,     ___,     ___,        ___,        ___,  ___,  ___,     KC_UP,   ___,     ___,  ARING,
+    ___,      KC_LBRC, KC_RBRC, S(KC_LBRC), S(KC_RBRC), ___,  ___,  KC_LEFT, KC_DOWN, KC_DOWN, ODIA, ADIA,
+    ___,      S(KC_9), S(KC_0), S(KC_COMM), S(KC_DOT),  ___,  ___,  ___,     ___,     ___,     ___,  ___,
+    ___,      ___,     ___,     ___,        ___,        ___,  ___,  ___,     ___,     ___,     ___,  ___
   ),
 
   [_THREE] = LAYOUT_ortho_4x12(
-    ___,      ___,     ___,     ___,     ___,     ___,  ___,  ___,  ___,  ___,  ___,  ___,
-    ___,      RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, ___,  ___,  ___,  ___,  ___,  ___,  RESET,
-    ___,      BL_TOGG, BL_STEP, ___,     ___,     ___,  ___,  ___,  ___,  ___,  ___,  ___,
-    ___,      ___,     ___,     ___,     ___,     ___,  ___,  ___,  ___,  ___,  ___,  ___
+    ___,      ___,     ___,     ___,     ___,     ___,  ___,  ___,       KC__VOLUP,    ___,  ___,  ___,
+    ___,      RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, ___,  ___,  KC__MUTE,  KC__VOLDOWN,  ___,  ___,  RESET,
+    ___,      BL_TOGG, BL_STEP, ___,     ___,     ___,  ___,  ___,       ___,          ___,  ___,  ___,
+    ___,      ___,     ___,     ___,     ___,     ___,  ___,  ___,       ___,          ___,  ___,  ___
   ),
 
   [_FOUR] = LAYOUT_ortho_4x12(
