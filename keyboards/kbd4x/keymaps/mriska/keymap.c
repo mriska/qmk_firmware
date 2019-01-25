1d2009c329225780e28f6df613d148bb0c76cc47
Mikael Riska <mikael.riska@ecraft.com>
2019-01-25 09:38:49 +0100

    tidy up keycodes. add tilde

--- keyboards/kbd4x/keymaps/mriska/keymap.c
+++ keyboards/kbd4x/keymaps/mriska/keymap.c
@@ -41,17 +41,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_ARROW] = LAYOUT_ortho_4x12(
-    ___,      ___,     ___,     ___,        ___,        ___,  ___,  ___,     KC_UP,   ___,     ___,  AA,
-    ___,      KC_LBRC, KC_RBRC, S(KC_LBRC), S(KC_RBRC), ___,  ___,  KC_LEFT, KC_DOWN, KC_RGHT, OE,   AE,
-    ___,      S(KC_9), S(KC_0), S(KC_COMM), S(KC_DOT),  ___,  ___,  ___,     ___,     ___,     ___,  ___,
-    ___,      ___,     ___,     ___,        ___,        ___,  ___,  ___,     ___,     ___,     ___,  ___
+    ___, KC_LPRN, KC_RPRN, KC_LT,   KC_GT,   ___,  ___,  ___,     KC_UP,   ___,     ___,  AA,
+    ___, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, ___,  ___,  KC_LEFT, KC_DOWN, KC_RGHT, OE,   AE,
+    ___, ___,     ___,     ___,     ___,     ___,  ___,  ___,     ___,     ___,     ___,  ___,
+    ___, ___,     ___,     ___,     ___,     ___,  ___,  ___,     ___,     ___,     ___,  ___
   ),
 
   [_NUM] = LAYOUT_ortho_4x12(
-    S(KC_EQUAL), S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), KC_MINUS,
-    KC_EQUAL,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    S(KC_MINUS),
-    KC_GRAVE,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  ___,
-    ___,         ___,     ___,     ___,     ___,     KC_DEL,  ___,     ___,     ___,     ___,     ___,     ___
+    KC_PLUS,  KC_EXLM, KC_AT, KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINUS,
+    KC_EQUAL, KC_1,    KC_2,  KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_UNDS,
+    KC_GRAVE, KC_F1,   KC_F2, KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TILD,
+    ___,      ___,     ___,   ___,     ___,     KC_DEL,  ___,     ___,     ___,     ___,     ___,     ___
   ),
 
   [_THREE] = LAYOUT_ortho_4x12(
