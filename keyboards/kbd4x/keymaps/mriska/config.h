020b62018bf047719ff5fd562be7429ad8daa91a
Mikael Riska <mikael.riska@ecraft.com>
2019-02-25 10:51:58 +0100

    Add mouse support

--- keyboards/kbd4x/keymaps/mriska/config.h
+++ keyboards/kbd4x/keymaps/mriska/config.h
@@ -17,3 +17,9 @@
 #pragma once
 
 // place overrides here
+#define MOUSEKEY_DELAY             100
+#define MOUSEKEY_INTERVAL          30
+#define MOUSEKEY_MAX_SPEED         10
+#define MOUSEKEY_TIME_TO_MAX       20
+#define MOUSEKEY_WHEEL_MAX_SPEED   8
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 40
