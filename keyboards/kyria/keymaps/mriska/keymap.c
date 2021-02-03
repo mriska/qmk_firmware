99cf00088f1a89c8206877f3e975e93074c5d095
Mikael Riska <mikael.riska@ecraft.com>
2021-02-03 12:38:28 +0100

    layout change

--- keyboards/kyria/keymaps/mriska/keymap.c
+++ keyboards/kyria/keymaps/mriska/keymap.c
@@ -45,10 +45,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [QWERTY] = LAYOUT(
-        KC_ESC,  KC_Q,        KC_W,         KC_E,         KC_R,         KC_T,                                              KC_Y,    KC_U,         KC_I,         KC_O,         KC_P,            KC_QUOT,
-        KC_TAB,  SFT_T(KC_A), LCTL_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), KC_G,                                              KC_H,    RGUI_T(KC_J), RALT_T(KC_K), RCTL_T(KC_L), RSFT_T(KC_SCLN), KC_ENTER,
-        KC_HOME, KC_Z,        KC_X,         KC_C,         KC_V,         KC_B,    KC_LGUI,  XXXXXXX,  XXXXXXX,   KC_LEAD,   KC_N,    KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_END,
-                                            XXXXXXX,      KC_LALT,      KC_LGUI, LT(LOWER, KC_BSPC), TG(LOWER), TG(RAISE), LT(RAISE, KC_SPACE), KC_LSFT, KC_LCTL, XXXXXXX
+        KC_ESC,  KC_Q,        KC_W,         KC_E,         KC_R,         KC_T,                                                     KC_Y,      KC_U,         KC_I,         KC_O,         KC_P,            KC_QUOT,
+        KC_TAB,  SFT_T(KC_A), LCTL_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), KC_G,                                                     KC_H,      RGUI_T(KC_J), RALT_T(KC_K), RCTL_T(KC_L), RSFT_T(KC_SCLN), KC_ENTER,
+        KC_HOME, KC_Z,        KC_X,         KC_C,         KC_V,         KC_B,    KC_LGUI,            XXXXXXX, XXXXXXX,  KC_LEAD,  KC_N,      KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_END,
+                                            XXXXXXX,      TG(LOWER),    KC_LGUI, LT(LOWER, KC_BSPC), _______, _______, LT(RAISE, KC_SPACE), XXXXXXX,      TG(RAISE),    XXXXXXX
     ),
 
     [LOWER] = LAYOUT(
